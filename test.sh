7941617232c6cd043bda5caa2e3212e58d79f8dc
gospodin66 <borna986@gmail.com>
2023-03-21 21:53:42 +0100

    .

--- test.sh
+++ test.sh
@@ -1,14 +1,28 @@
 #!/bin/sh
 
-echo "Testing.."
+user=$1
 
-python3 crypt.py ljkra "This is test data"
+if [ -z "$user" ]; then
+    echo "Missing user arg"
+    exit 1
+fi
 
-python localrsa.py testuser0 "This is sample data" /home/cheki/projects/py_aes_rsa/ENCRYPTED-data/testuser0_encrypted_data.bin
-python localaes.py testuser0 "This is some sample data"
+echo "Testing crypt.py for user: $user"
+python3 crypt.py "$user" "This is test data"
+echo "Testing localrsa.py for user: $user"
+python localrsa.py "$user" "This is sample data" /home/cheki/projects/py_aes_rsa/ENCRYPTED-data/"$"user_encrypted_data.bin
+echo "Testing localaes.py for user: $user"
+python localaes.py "$user" "This is some sample data"
+echo "Testing rsa-encrypt.py for user: $user"
+python rsa-encrypt.py "$user"
+echo "Testing rsa-decrypt-aes-encrypt.py for user: $user"
+python rsa-decrypt-aes-encrypt.py "$user" "This is classified!"
+echo "Testing rsa-decrypt-aes-decrypt.py for user: $user"
+python rsa-decrypt-aes-decrypt.py "$user"
+echo "Testing crypt.py [jpg] for user: $user"
+python3 crypt.py "$user" assets/example.jpg
+echo "Testing crypt.py [gif] for user: $user"
+python3 crypt.py "$user" assets/example.gif
 
-python rsa-encrypt.py testuser0
-python rsa-decrypt-aes-encrypt.py testuser0 "This is classified!"
-python rsa-decrypt-aes-decrypt.py testuser0
-
-python3 crypt.py ljkra assets/c.jpg
+echo "Done"
+exit 0
